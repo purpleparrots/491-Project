46e0d869b50eca261a8e81eba975821cd9658f40
purpleparrots <purpleparrots491@gmail.com>
2015-02-17 20:11:03 -0800

    Update GameEngine.js

--- GameEngine.js
+++ GameEngine.js
@@ -24,8 +24,8 @@ function GameEngine() {
     this.count = 0;
     this.ship = null;
     this.typeMap = {};
-    this.liveLocationX = 35;
-    this.sliderLocationX = null;
+    this.liveLocationX = 35;        
+    this.sliderLocationX = null; 
 }
 
 GameEngine.prototype.init = function (game_ctx, background_ctx, overlay_ctx) {
@@ -37,55 +37,65 @@ GameEngine.prototype.init = function (game_ctx, background_ctx, overlay_ctx) {
     this.startInput();
     this.timer = new Timer();
     this.wave = 1;
+
+    for(var i = 0; i < 100; i++) {  39      //this.makeProtoEnemies(); 
+              if(i < 20) this.typeMap[i] = "fillShieldPowerUp";       
+              if(i >= 20 && i < 30) this.typeMap[i] = "extraLifePowerUp";         
+              if(i >= 30 && i < 50) this.typeMap[i] = "doubleGunPowerUp";         
+              if(i >= 50 && i < 60) this.typeMap[i] = "tripleGunPowerUp";         
+              if(i >= 60 && i < 78) this.typeMap[i] = "backGunPowerUp";       
+              if(i >= 78 && i < 100) this.typeMap[i] = "bombPowerUp";         
+      } 
     // temp function to show basic animations for prototype. remove for final
-    this.makeProtoEnemies();
+    //this.makeProtoEnemies();
 }
 
 GameEngine.prototype.start = function () {
-	//create 3 lives
-	this.createLife();
-	this.createLife();
-	this.createLife();
-	
-	//create shield bar and slider
-	this.resetSlider();
-	
-	//create and display score
-	this.overlay_ctx.font="15px Georgia";
-	this.overlay_ctx.fillStyle = "white";
-	this.overlay_ctx.fillText("Score: ", 480, 360);
-	this.overlay_ctx.fillText("" + this.score + "", 525, 360);
-	
-	var that = this;
+
+    var that = this;
+    this.createLife();      
+      this.createLife();      
+      this.createLife();      
+              
+      //create shield bar and slider      
+      this.resetSlider();         
+              
+      //create and display score      
+      this.overlay_ctx.font="15px Georgia";       
+      this.overlay_ctx.fillStyle = "white";       
+      this.overlay_ctx.fillText("Score: ", 480, 360);         
+      this.overlay_ctx.fillText("" + this.score + "", 525, 360);
     (function gameLoop() {
         that.loop();
         requestAnimFrame(gameLoop, that.game_ctx.canvas);
     })();
+    document.title = this.wave;
+    this.generateWave();
 }
 
-GameEngine.prototype.createLife = function() {
-	this.overlay_ctx.drawImage(AM.getAsset("./images/playership.png"), this.overlay_ctx.canvas.width - this.liveLocationX, 5, 30, 30);
-	this.liveLocationX += 35;
-}
-
-GameEngine.prototype.removeLife = function() {
-	this.liveLocationX -= 35;
-	this.overlay_ctx.clearRect(this.overlay_ctx.canvas.width - this.liveLocationX, 5, 30, 30);
-}
-
-GameEngine.prototype.resetSlider = function() {
-	this.overlay_ctx.clearRect(this.sliderLocationX - 10, this.overlay_ctx.canvas.height - 80, 20, 70);
-	this.sliderLocationX = 450;
-	this.overlay_ctx.drawImage(AM.getAsset("./images/shieldbar.jpg"), this.surfaceWidth/2, this.overlay_ctx.canvas.height - 60, this.surfaceWidth, 30);
-	this.overlay_ctx.drawImage(AM.getAsset("./images/slider.png"), this.sliderLocationX, this.overlay_ctx.canvas.height - 70, 10, 50);
-}
-
-GameEngine.prototype.moveSlider = function(amount) {
-	this.overlay_ctx.clearRect(this.sliderLocationX - 10, this.overlay_ctx.canvas.height - 80, 20, 70);
-	this.overlay_ctx.drawImage(AM.getAsset("./images/shieldbar.jpg"), this.surfaceWidth/2, this.overlay_ctx.canvas.height - 60, this.surfaceWidth, 30);
-	this.sliderLocationX = this.sliderLocationX - (amount * 3);
-	this.overlay_ctx.drawImage(AM.getAsset("./images/slider.png"), this.sliderLocationX, this.overlay_ctx.canvas.height - 70, 10, 50);
-}
+GameEngine.prototype.createLife = function() {      
+      this.overlay_ctx.drawImage(AM.getAsset("./images/playership.png"), this.overlay_ctx.canvas.width - this.liveLocationX, 5, 30, 30);      
+      this.liveLocationX += 35;       
+  }       
+          
+GameEngine.prototype.removeLife = function() {      
+      this.liveLocationX -= 35;       
+      this.overlay_ctx.clearRect(this.overlay_ctx.canvas.width - this.liveLocationX, 5, 30, 30);      
+  }       
+          
+GameEngine.prototype.resetSlider = function() {         
+      this.overlay_ctx.clearRect(this.sliderLocationX - 10, this.overlay_ctx.canvas.height - 80, 20, 70);         
+      this.sliderLocationX = 450;         
+      this.overlay_ctx.drawImage(AM.getAsset("./images/shieldbar.jpg"), this.surfaceWidth/2, this.overlay_ctx.canvas.height - 60, this.surfaceWidth, 30);         
+      this.overlay_ctx.drawImage(AM.getAsset("./images/slider.png"), this.sliderLocationX, this.overlay_ctx.canvas.height - 70, 10, 50);      
+  }       
+          
+GameEngine.prototype.moveSlider = function(amount) {        
+      this.overlay_ctx.clearRect(this.sliderLocationX - 10, this.overlay_ctx.canvas.height - 80, 20, 70);         
+      this.overlay_ctx.drawImage(AM.getAsset("./images/shieldbar.jpg"), this.surfaceWidth/2, this.overlay_ctx.canvas.height - 60, this.surfaceWidth, 30);         
+      this.sliderLocationX = this.sliderLocationX - (amount * 3);         
+      this.overlay_ctx.drawImage(AM.getAsset("./images/slider.png"), this.sliderLocationX, this.overlay_ctx.canvas.height - 70, 10, 50);      
+  } 
 
 GameEngine.prototype.startInput = function () {
     var that = this;
@@ -115,9 +125,9 @@ GameEngine.prototype.draw = function () {
     this.game_ctx.save();
     for (var i = 0; i < this.entities.length; i++) {
         if (this.entities[i].removeMe) {
-        	this.score += this.entities[i].value;
-        	this.overlay_ctx.clearRect(520, 345, 200, 100);
-            this.overlay_ctx.fillText("" + this.score + "", 525, 360);
+            this.score += this.entities[i].value;   80              this.increment("score", this.entities[i].value); 
+            this.overlay_ctx.clearRect(520, 345, 200, 100);         
+            this.overlay_ctx.fillText("" + this.score + "", 525, 360); 
             this.entities.splice(i,1);
         } else {
             this.entities[i].draw(this.ctx);
@@ -166,7 +176,7 @@ GameEngine.prototype.absoluteDistance = function(entity1, entity2) {
 GameEngine.prototype.loop = function () {
     this.clockTick = this.timer.tick();
     // reenable waveTick incrememnt went moving past prototype
-    //this.waveTick += 1;
+    this.waveTick += 1;
 
     // 500x + 8000
     // first wave gets 26 waves points. if each of these is an asteroid that means
@@ -174,9 +184,10 @@ GameEngine.prototype.loop = function () {
     // seconds to kill all, this equals 7800 ticks, round to 8000 and add 500 ticks
     // to each wave. wave points increase by 15 each time or about 3x the increased time
     // so the game gets harder in several ways each wave.
-    if (this.waveTick > (500 * this.wave) + 8000) {
+    if (this.waveTick > (75 * this.wave) + 300) {
         this.waveTick = 0;
-        this.increment("wave",1);
+        this.wave += 1;
+        document.title = this.wave;
         this.generateWave();
     }
 
@@ -217,6 +228,7 @@ GameEngine.prototype.changeState = function() {
 }
 
 GameEngine.prototype.generateWave = function() {
+        
     //points worth of enemies generated this wave.
     waveValue = (this.wave * 15) + 11;
     //chance an alien can spawn. this is < 0 until wave 3.
@@ -224,34 +236,31 @@ GameEngine.prototype.generateWave = function() {
     //chance a powerup can spawn. this is < 0 until wave 2.
     powerupChance = (this.wave * 2) - 2;
     while (waveValue > 0) {
-        type = this.getRandomInt(1,100);
-        velocity = {x: this.getRandomInt(-4,4), y: this.getRandomInt(-4,4)};
-        size = this.getRandomInt(1,3);
-        angle = Math.random() * Math.PI;
-        x = this.randOffScreenPoint();
-        y = this.randOffScreenPoint();
-/*
-        if (type - alienChance > 0) {
+        var type = this.getRandomInt(1,100);
+        var velocity = {x: this.getRandomInt(-4,4), y: this.getRandomInt(-4,4)};
+        var size = this.getRandomInt(1,3);
+        var angle = Math.random() * Math.PI;
+        var x = this.randOffScreenPoint();
+        var y = this.randOffScreenPoint();
+
+        if (type + alienChance > 95) {
             
             value = this.getRandomInt(2,4) * 10;
-            this.addEntity(new AlienShip(this, angle, velocity, null, x, y, null, value));
+
+            this.addEntity(new AlienShip(this, (Math.round() * 2 * Math.PI), velocity, AM.getAsset("./images/alienship.png"), x, y, null, 100, "default"));
             waveValue -= value;
 
         } else {
-            */
+            
             this.addEntity(new Asteroid(this, angle, velocity, x, y, size));
             waveValue -= size;
-        //}
-        /*
-        if (type - powerupChance > 0) {
-            velocity = {x: velx, y: vely};
-            this.addEntity(new PowerUp(this, angle, velocity, null, x, y, null));
         }
-        */
         
-
-  //      this.addEntity(new Asteroid(this, Math.random() * 2 * Math.PI, velocity, this.randOffScreenPoint(), this.randOffScreenPoint(), size));
-        waveValue -= size;
+        if (type + powerupChance > 95) {
+            var i = this.getRandomInt(0,100);
+            this.addEntity(new PowerUp(this, 2 * Math.PI,velocity, 100, 0, this.typeMap[i]));
+        }
+        
     }
 }
 
@@ -267,20 +276,18 @@ GameEngine.prototype.randOffScreenPoint = function() {
 }
 
 GameEngine.prototype.makeProtoEnemies = function() {
-    this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: -2, y: -1}, -100, 50, 3));
-    this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: 2, y: 1}, 100, 50, 3));
-    this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: this.getRandomInt(1,4), y: this.getRandomInt(1,4)}, -200, 200, 2));
-    this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: this.getRandomInt(1,4), y: this.getRandomInt(1,4)}, -100,-150, 3));
-    this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: this.getRandomInt(1,4), y: this.getRandomInt(1,4)},  200, 300, 1));
-    
-//    this.generateWave();
+    this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: -2, y: -1}, 100, -50, 3));
+    this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: 2, y: 1}, -100, 50, 2));
+    //this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: this.getRandomInt(1,4), y: this.getRandomInt(1,4)}, -200, 200, 2));
+    //this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: this.getRandomInt(1,4), y: this.getRandomInt(1,4)}, -100,-150, 3));
+    //this.addEntity(new Asteroid(this, (Math.random() * 2 * Math.PI), {x: this.getRandomInt(1,4), y: this.getRandomInt(1,4)},  200, 300, 1));
 
     //this.addEntity(new Asteroid(this, 0, {x: 0, y: 0}, 100, 25, 2));  
     //this.addEntity(new Asteroid(this, 0, {x: 0, y: 0}, -100, 25, 3));
-    this.addEntity(new PowerUp(this, 2 * Math.PI,{x:1, y:1}, 100, 0, "bombPowerUp"));
-    this.addEntity(new PowerUp(this, 2 * Math.PI,{x:-1, y:-1}, 150, 0, "extraLifePowerUp"));
+    this.addEntity(new PowerUp(this, 2 * Math.PI,{x:0, y:0}, 100, 0, "bombPowerUp"));
+    this.addEntity(new PowerUp(this, 2 * Math.PI,{x:0, y:0}, 150, 0, "bombPowerUp"));
     this.addEntity(new AlienShip(this, (Math.round() * 2 * Math.PI), {x:0, y:0}, AM.getAsset("./images/alienship.png"), -75, 0, null, 100, "default"));
-   
+    generateWave();
 }
 
 GameEngine.prototype.getRandomInt = function(min, max) {
@@ -300,6 +307,56 @@ GameEngine.prototype.resolveVec = function(angle, mag) {
     ret.y = mag * Math.sin(angle);
     return ret;
 }
+
+GameEngine.prototype.resolveCollision = function(entity1V, entity1M, entity2V, entity2M) {
+        /*var v1 = entity1V;
+        var v2 = entity2V;
+        var m1 = entity1M;
+        var m2 = entity2M;
+
+        v1.x = v1.x * (m1 - m2);
+        v1.y = v1.y * (m1 - m2);
+        v2.x = v2.x * 2 * m2;
+        v2.y = v2.y * 2 * m2;
+        var v3 = {
+            x: 0,
+            y: 0
+        };
+        v3.x = v1.x + v2.x;
+        v3.y = v1.y + v2.y;
+        v3.x = v3.x / (m1 + m2);
+        v3.y = v3.y / (m1 + m2);
+
+        v1 = entity1V;
+        v2 = entity2V;
+        m1 = entity1M;
+        m2 = entity2M;
+
+        v1.x = v1.x * (m1 - m2);
+        v1.y = v1.y * (m1 - m2);
+        v2.x = v2.x * 2 * m2;
+        v2.y = v2.y * 2 * m2;
+        var v4 = {
+            x: 0,
+            y: 0
+        };
+        v4.x = v1.x + v2.x;
+        v4.y = v1.y + v2.y;
+        v4.x = v4.x / (m1 + m2);
+        v4.y = v4.y / (m1 + m2);
+
+        return [v3, v4];
+        */
+        var v3 = {};
+        v3.x = (entity1V.x * (entity1M - entity2M) + 2 * entity2M * entity2V.x) / (entity1M + entity2M);      
+        v3.y = (entity1V.y * (entity1M - entity2M) + 2 * entity2M * entity2V.y) / (entity1M + entity2M);      
+        var v4 = {};      
+        v4.x = (entity2V.x * (entity2M - entity1M) + 2 * entity1M * entity1V.x) / (entity1M + entity2M);      
+        v4.y = (entity2V.y * (entity2M - entity1M) + 2 * entity1M * entity1V.y) / (entity1M + entity2M);      
+        console.log(v3);      
+        console.log(v4);      
+        return [v3, v4]; 
+    };
 
 GameEngine.prototype.velocityMag = function(vel) {
     return Math.sqrt(vel.x * vel.x + vel.y * vel.y);
