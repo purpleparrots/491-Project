eb7294d53f156474d8e9b37f0e4da505550df5a4
purpleparrots <alpertmd@gmail.com>
2015-03-04 14:29:16 -0800

    formatting

--- index.php
+++ index.php
@@ -3,19 +3,29 @@
 <html>
 <head>
 	<style>
-		table, th, td {
+		h1 {
+			font-size: 28px;
+			font-color: gray;
+			font-family: Impact;
+			text-align:center;
+		}
+		ul{
+			text-align:center;
+		}
+		li {
+			text-align:left;
+		}
+		table {
+			width:100%;
+		}
+		th,td {
 			font-family: Impact;
 			border: 1px solid black;
-			width: 100%;
-		}
-		th, td {
-			padding: 20px;
+			padding: 10px;
+			text-align: center;
 		}
 		thead{
-			background:grey;
-		}
-		th {
-			text-align: center;
+			background:gray;
 		}
 		#game{
 			float:left;
@@ -24,11 +34,16 @@
 			position: absolute;
 		}
 		#highscores{
-			float:right;
-			margin: 0px 100px 0px;
+			width: 20%;
+			margin: 0px 950px 0px;
 		}
-		#scores{
-			
+		#legend {
+		}
+		#control {
+			width:70%;
+		}
+		#powerup {
+			width:30%;
 		}
 	</style>
     <meta charset="utf-8">
@@ -39,19 +54,20 @@
 	<script type ="text/javascript" src ="./main.js"></script>
 	<script type ="text/javascript" src ="./SpaceObject.js"></script>
 	<script type= "text/javascript" src ="./GameEngine.js"></script>
-	
-	
+
+      <h1>League of Asteroids: Destruction of the Asteroids (DOTA)</h1>
 	
 </head>
 <body>
+   
     <div id="game">
  		<canvas left="0px" top="0px" id="next_background" tabindex="0" width="900" height="600" z-index="-2"></canvas>
  		<canvas left="0px" top="0px" id="background" tabindex="2" width="900" height="600" z-index="-1"></canvas>
      	<canvas left="0px" top="0px"id="gameWorld" tabindex="1" width="900" height="600" z-index="0"></canvas>
  		<canvas left="0px" top="0px"id="overlay" tabindex="3" width="900" height="600" z-index="1"></canvas>
 	</div>
-	<div id="highscores">
-		<table id="scores">
+	<div>
+		<table id="highscores">
 			<thead>
 				<tr>
 					<th colspan=2><h1>High Scores</h1></th>
@@ -63,7 +79,7 @@
 				$password = "yofer`";
 				$hostname = "repos.insttech.washington.edu";
 				
-				$dbhandle = mysql_connect($hostname, $username, $password) or die("Unable 					to connect to MySQL");
+				$dbhandle = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
 				
 				$selected = mysql_select_db("alpertmd", $dbhandle) or die("Could not select high score db");
 				
@@ -82,5 +98,61 @@
 			</tbody>
 		</table>
 	</div>
+	<br>
+	<br>
+	<br>
+	<table id="legend">
+			 <thead>
+				 <tr>
+   		 			 <th id="control"><h1>Controls</h1></th>
+					 <th id="powerup" colspan="2"><h1>PowerUps</h1></th>
+				 </tr>						 
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+			   			 <h2>
+			       			 <ul>
+								 <li>Click the game window to start and to pause</li>
+			       				 <li>Up Arrow / Down Arrow - thrust</li>
+			   					 <li>Left Arrow / Right Arrow - rotate ship </li>
+			   					 <li>Space Bar - Fire main weapon</li>
+			   					 <li>CTRL - Fire secondary weapon</li>
+			   				 </ul>
+						 </h2>
+					</td>
+					
+					<td>
+						<table>
+				
+							<tr>
+								<td><img src="./images/red_crystal.png" /></td>
+								<td>Bomb Upgrade</td>
+							</tr>
+							<tr>
+								<td><img src="./images/blue_crystal.png" /></td>
+								<td>Shield Upgrade</td>
+							</tr>
+							<tr>
+								<td><img src="./images/tan_crystal.png" /></td>
+								<td>Double Shot</td>
+							</tr>
+							<tr>
+								<td><img src="./images/brown_crystal.png" /></td>
+								<td>Triple Shot</td>
+							</tr>
+							<tr>
+								<td><img src="./images/lime_green_crystal.png" /></td>
+								<td>+1 Life</td>
+							</tr>
+							<tr>
+								<td><img src="./images/purple_crystal.png" /></td>
+								<td>Rear Shot</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</tbody>
+		</table>
 </body>
 </html>
